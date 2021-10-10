10f36c27add440ea3c0da0b75f6c6f433a7e7ca3
weijchen0919 <weijchen0919@gmail.com>
2021-10-10 14:19:49 -0400

    remove useless code

--- Assets/Transporter.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Transporter : MonoBehaviour
-{
-    // [SerializeField] private transporterFlyingPath[] _transporterFlyingPaths;
-}
